38b0d0cdf692409503e30496a35a93fa334dca62
Zana Canpolat <zana_canpolat@hotmail.se>
2025-01-09 17:42:31 +0100

    Added while loop user can perform several operations before exiting

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -8,32 +8,39 @@ public class Main {
         //variable declarations
         Scanner terminalInput = new Scanner(System.in);
         int choice;
+        boolean exit = false;
 
-        // Printing user choice
-        System.out.println("Calculator");
-        System.out.println("Addition        + : 1");
-        System.out.println("Subtraction     - : 2");
-        System.out.println("Multiplication  * : 3");
-        System.out.println("Division        / : 4");
-        System.out.println("Exit: 5");
-        System.out.println("Choose one of the following operation: ");
-        choice = terminalInput.nextInt();
 
-        //The following code checks user input and calls correct method
-        if (choice == 1) {
-            System.out.println("Addition: ");
-            addition();
-        } else if (choice == 2) {
-            System.out.println("Subtraction: ");
-            subtraction();
-        } else if (choice == 3) {
-            System.out.println("Multiplication: ");
-            multiplication();
-        } else if (choice == 4) {
-            System.out.println("Division: ");
-            division();
-        } else {
-            System.out.println("Closing calculator! ");
+        while (!exit) {
+
+            // Printing user choice
+            System.out.println("Calculator");
+            System.out.println("Addition        + : 1");
+            System.out.println("Subtraction     - : 2");
+            System.out.println("Multiplication  * : 3");
+            System.out.println("Division        / : 4");
+            System.out.println("Exit: 5");
+            System.out.println("Choose one of the following operation: ");
+            choice = terminalInput.nextInt();
+
+            //The following code checks user input and calls correct method
+            if (choice == 1) {
+                System.out.println("Addition: ");
+                addition();
+            } else if (choice == 2) {
+                System.out.println("Subtraction: ");
+                subtraction();
+            } else if (choice == 3) {
+                System.out.println("Multiplication: ");
+                multiplication();
+            } else if (choice == 4) {
+                System.out.println("Division: ");
+                division();
+            } else {
+                System.out.println("Closing calculator! ");
+                exit = true;
+
+            }
         }
     }
 
@@ -47,33 +54,31 @@ public class Main {
     }
 
     // Method for subtraction, takes two integer and subracts
-    static void subtraction(){
+    static void subtraction() {
         Scanner terminalInput = new Scanner(System.in); //Check if it is possible to declare global so that we can remove this line of code
         System.out.println("Enter two numbers");
         int num1 = terminalInput.nextInt();
         int num2 = terminalInput.nextInt();
-        System.out.println("Result: " +(num1-num2));
+        System.out.println("Result: " + (num1 - num2));
     }
 
     // Method for multiplication takes two integer and multiply
-    static void multiplication(){
+    static void multiplication() {
         Scanner terminalInput = new Scanner(System.in); //Check if it is possible to declare global so that we can remove this line of code
         System.out.println("Enter two numbers");
         int num1 = terminalInput.nextInt();
         int num2 = terminalInput.nextInt();
-        System.out.println("Result: " +(num1*num2));
+        System.out.println("Result: " + (num1 * num2));
     }
 
     // Method for division takes two integer and devides
-    static void division(){
+    static void division() {
         Scanner terminalInput = new Scanner(System.in); //Check if it is possible to declare global so that we can remove this line of code
         System.out.println("Enter two numbers");
         int num1 = terminalInput.nextInt();
         int num2 = terminalInput.nextInt();
-        System.out.println("Result: " +(num1/num2));
+        System.out.println("Result: " + (num1 / num2));
     }
-
-
 
 }
 
